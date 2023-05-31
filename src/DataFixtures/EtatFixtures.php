032b42ab78853be2a21aa44477c723573d98fdab
Irwin Le Bihan <irwnin.lebihan2023@campus-eni.fr>
2023-05-31 11:38:29 +0200

    EtatFixtures OK

--- src/DataFixtures/EtatFixtures.php
+++ src/DataFixtures/EtatFixtures.php
@@ -2,19 +2,35 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Etat;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class EtatFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-
+        $this->createEtat( $manager);
 
         $manager->flush();
     }
 
+    public function createEtat(ObjectManager $manager)
+    {
+        $param = ['Créée', 'Ouverte',
+'Clôturée', 'Activité en
+cours', 'Passée', 'Annulée','Archivée'];
 
+        for ($i=0; $i<7;$i++) {
+            $etat = new Etat();
+            $etat->setLibelle($param[$i]);
+            $manager->persist($etat);
+        }
+
+
+
+}
 
 
 }
--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -90,7 +90,7 @@ class UserFixtures extends Fixture
                 ->setActif((bool)$generator->randomElements(['true', 'false']))
                 ->setPassword($this->hasher->hashPassword($user, $generator->password));
 
-            dump($user);
+
 
             $manager->persist($user);
 
