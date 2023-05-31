e60891f9b1879a1c37a93a4f85d05dfe823ad935
mbouaphomma <m.bouaphomma@laposte.net>
2023-05-31 11:07:53 +0200

    commit fixture ville

---
+++ src/DataFixtures/VillesFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Ville;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class VillesFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $this->addVille($manager);
+
+        $manager->flush();
+    }
+
+    public function addVille(ObjectManager $manager){
+
+        $generator = Factory::create('fr_FR');
+
+        for ($i=0; $i<20; $i++ ){
+            $ville = new Ville();
+            $ville
+                ->setNom($generator->city)
+                ->setCodePostal($generator->numberBetween(10000,90000));
+
+            $manager->persist($ville);
+        }
+        $manager->flush($ville);
+    }
+
+}
