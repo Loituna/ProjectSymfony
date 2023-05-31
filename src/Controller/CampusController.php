1da6394b57b3e34d9c175335525e59ba6c10f8f6
Johanna Peignelin <johanna.peignelin2023@campus-eni.fr>
2023-05-31 15:07:02 +0200

    add LieuFixtures

--- src/Controller/CampusController.php
+++ src/Controller/CampusController.php
@@ -2,20 +2,41 @@
 
 namespace App\Controller;
 
+use App\Entity\Campus;
+use App\Form\CampusType;
+use App\Repository\CampusRepository;
+use App\Tools\Uploader;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CampusController extends AbstractController
 {
     #[Route('/campus', name: 'app_campus')]
-    public function index(): Response
+    public function index(CampusRepository $campusRepository,
+                            Request $request,
+                            EntityManagerInterface $entityManager): Response
     {
         //Renvoyer une liste/tableau des campus rentrés dans ma BDD
+        $listCampus = $campusRepository->findAll();
 
+        $campus = new Campus();
+        //créer un formulaire pour Campus
+        $campusForm = $this->createForm(CampusType::class,$campus);
+        $campusForm->handleRequest($request);
 
-        return $this->render('campus/index.html.twig', [
-            'controller_name' => 'CampusController',
+        if ($campusForm->isSubmitted() && $campusForm->isValid()){
+            $campusRepository->save($campus, true);
+
+            $this->addFlash('success', 'Campus bien rajouté');
+
+            return $this->redirectToRoute('app_campus');
+        }
+        return $this->render('campus/indexCampus.html.twig', [
+            'campus' => $listCampus,
+            'campusForm' =>$campusForm->createView()
         ]);
     }
 
---
+++ src/Form/CampusType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Campus;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class CampusType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'required' => true,
+                'label' => "Nom du nouveau Campus",
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Veuillez saisir un nom"
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Le nom du Campus doit contenir au minimum {{ limit }} caractères'
+                    ])
+                ]
+            ])
+            ->add('save',SubmitType::class, [
+                'label' => 'Valider'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Campus::class,
+        ]);
+    }
+}
