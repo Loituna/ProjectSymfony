1c3c295c0f046cdab19598d101e8ecb70ca459a8
Irwin Le Bihan <irwnin.lebihan2023@campus-eni.fr>
2023-06-01 15:19:43 +0200

    Ajout Controlleur Admin/Lieu

---
+++ src/Controller/LieuController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+#[Route('/lieu', name: 'lieu_')]
+class LieuController extends AbstractController
+{
+    #[Route('/{id}', name: '_detail')]
+    public function index(): Response
+    {
+        return $this->render('lieu/index.html.twig', [
+            'controller_name' => 'LieuController',
+        ]);
+    }
+}
