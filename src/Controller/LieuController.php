7beb2896135d86619a2b1ddc952965483149acc6
Irwin Le Bihan <irwnin.lebihan2023@campus-eni.fr>
2023-06-02 14:15:08 +0200

    Ajout d'un Lieu depuis Ville

--- src/Controller/LieuController.php
+++ src/Controller/LieuController.php
@@ -6,19 +6,40 @@ use App\Entity\Lieu;
 use App\Form\LieuType;
 use App\Repository\LieuRepository;
 use App\Repository\VilleRepository;
-use http\Env\Request;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 #[Route('/lieu', name: 'lieu_')]
 class LieuController extends AbstractController
 {
     #[Route('/{id}', name: 'detailByVille', requirements: ['id' => '\d+'])]
-    public function detailByVille(LieuRepository $lieuRepository,VilleRepository $villeRepository , int $id ): Response
+    public function detailByVille(LieuRepository $lieuRepository,VilleRepository $villeRepository , int $id,Request $request, ): Response
     {
+        //Affichage
         $ville = $villeRepository->find($id);
         $lieus=$lieuRepository->findBy(['ville'=>$id]);
-        return $this->render('lieu/detailByVille.html.twig', [ 'lieus'=>$lieus , 'ville'=> $ville]);
+
+        //Formulaire
+        $newlieu = new Lieu();
+        $lieuForm = $this->createForm(LieuType::class,$newlieu);
+
+        //Traitement
+        $lieuForm->handleRequest($request);
+        if ($lieuForm->isSubmitted()&& $lieuForm->isValid()){
+            $newlieu->setVille($ville);
+            $lieuRepository->save($newlieu,true);
+            $this->addFlash('success', 'Lieu ajoutée');
+
+            return $this->redirectToRoute('lieu_detailByVille',['id' => $id]);
+
+
+
+        }
+
+
+        return $this->render('lieu/detailByVille.html.twig', [ 'lieus'=>$lieus , 'ville'=> $ville , 'lieuForm' => $lieuForm->createView()]);
     }
     #[Route('/delete/{id}', name: 'delete', requirements: ['id' => '\d+'])]
     public function deleteLieu(LieuRepository $lieuRepository,VilleRepository $villeRepository , int $id ): Response
--- src/Form/LieuType.php
+++ src/Form/LieuType.php
@@ -4,19 +4,49 @@ namespace App\Form;
 
 use App\Entity\Lieu;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class LieuType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('rue')
-            ->add('lattitude')
-            ->add('longitude')
-            ->add('ville')
+            ->add('nom',TextType::class, [
+        'required' => true,
+        'label' => "Ajouter un nouveau nom de Lieu",
+        'constraints' => [
+            new NotBlank([
+                'message' => "Veuillez saisir un nom"
+            ]),
+            new Length([
+                'min' => 5,
+                'minMessage' => 'Le nom du lieu doit contenir au minimum {{ limit }} caractères'
+            ])],      'attr' => ['class'=>'form-control']
+
+            ])
+            ->add('rue',TextType::class, [
+                'required' => true,
+                'label' => "Ajouter une Addrese pour le lieu",
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Veuillez saisir une adresse"
+                    ]),
+                    new Length([
+                        'min' => 10,
+                        'minMessage' => 'L\'adresse  nom du lieu doit contenir au minimum {{ limit }} caractères'
+                    ])],      'attr' => ['class'=>'form-control']
+
+            ])->add('save',SubmitType::class, [
+                'label' => 'Valider',
+                'attr' => ['class'=> 'btn btn-success mt-2 ']])
+
+
+
         ;
     }
 
